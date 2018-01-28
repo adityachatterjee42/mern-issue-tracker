6d88635707415dcb8aa650b60ecb4cd4a342a903
adityachatterjee <adityachatterjee42@gmail.com>
2018-01-28 15:02:08 -0800

    updates

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
-'use strict';
+'use strict'
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const Issue = require('./issue.js');
+
 const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
-
-
 
 app.get('/api/issues', (req, res) => {
   db.collection('issues').find().toArray().then(issues => {
@@ -20,26 +19,26 @@ app.get('/api/issues', (req, res) => {
   });
 });
 
-
 app.post('/api/issues', (req, res) => {
   const newIssue = req.body;
   newIssue.created = new Date();
-  if(!newIssue.status)
+  if (!newIssue.status)
     newIssue.status = 'New';
+
   const err = Issue.validateIssue(newIssue);
-  if(err){
-    res.status(442).json({message: 'Invalid request:${err}'});
+  if (err) {
+    res.status(422).json({ message: `Invalid request: ${err}` });
     return;
   }
-  db.collection('issues').insertOne(newIssue).then(result=>
-    db.collection('issues').find({_id:result.insertedId}).limit(1).next()
-).then(newIssue=>{
-  res.json(newIssue);
-}).catch(error=>{
-  console.log(error);
-  res.status(500).json({message: 'Internal server error: ${error}'});
-  });
 
+  db.collection('issues').insertOne(newIssue).then(result =>
+    db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
+  ).then(newIssue => {
+    res.json(newIssue);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
 });
 
 let db;
