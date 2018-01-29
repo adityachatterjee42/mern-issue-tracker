29aef5c8925d91243a004528c9bcd1c20727d45f
adityachatterjee <adityachatterjee42@gmail.com>
2018-01-29 15:49:36 -0800

    fixed webpack dev server bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
+const webpack = require('webpack');
+
 module.exports = {
-    entry: './src/App.jsx',
-    output: {
-      path: __dirname+'/static',
-      filename: 'app.bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx$/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['react','es2015']
-          }
-        },
-      ]
+  entry: {
+    app: './src/App.jsx',
+    vendor: ['react','react-dom','whatwg-fetch','babel-polyfill'],
+  },
+  output: {
+    path: __dirname+'./static',
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({name:'vendor',filename:'vendor.bundle.js'})
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react','es2015']
+        }
+      },
+    ]
+  },
+  devServer: {
+    port: 8000,
+    contentBase: 'static',
+    proxy:{
+      '/api/*':{
+        target: 'http://localhost:3000'
+      }
     }
-  };
+  }
+};
